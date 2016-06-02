9c2b62c4c8bfe6aa5a3641eb45936e2386fc81a6
Shawn Hu <shawnghu@gmail.com>
2016-06-02 14:48:14 -0500

    play in a given column added- example code at the bottom of Board.py should be self explanatory

--- .idea/Board.py
+++ .idea/Board.py
@@ -1,6 +1,8 @@
 class Board(object):
     def __init__(self, rows, columns):
         self.board = [['_' for x in range (columns)] for y in range (rows)]
+        self.rows = rows
+        self.columns = columns
 
 
     def __str__(self):
@@ -11,5 +13,20 @@ class Board(object):
             st += '\n'
         return st
 
+    def play(self, column, char):
+        #in this method, rownum starts counting from the bottom
+        for rownum in range (self.rows):
+            if self.board[self.rows - 1 - rownum][column] == '_':
+                self.board[self.rows - 1 - rownum][column] = char
+                return
+        #flow only reaches this point if none of the entries in that column is empty
+        print ('Invalid play- column is full')
+
+
 board = Board(6, 7)
-print board
+board.play(1, 'x')
+board.play(1, 'x')
+board.play(1,'o')
+board.play(2, 'o')
+board.play(3, 'x')
+print board.board
