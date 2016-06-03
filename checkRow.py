df4ce74d0f60c6a81161c4330f178a8f7aa9c690
ccorales95 <ccorales95@tamu.edu>
2016-06-03 10:09:46 -0500

    Check row
    Checks the rows diagonally, vertically, and horizontally

---
+++ checkRow.py
@@ -0,0 +1,57 @@
+import Board
+
+'''class checkRow:
+    # board: represents the list within a list structure
+    # player: identifies who is making the move
+    # k: the number that you are looking for diagonally, vertically, and horizonatlly
+    def __init__(self, board, player, k):
+        self.board = board
+        self.player = player
+        self.k = k
+
+    # compute_k takes the list within a list and then goes through it to check and see
+    # how many of the constant k there are'''
+
+def compute_k(board, k, player):
+    player_count = 0
+    k_count = 0
+
+    #check horizontally
+    for i in range(rows):
+        for j in range(columns):
+            if board.board[i][j] == player:
+                player_count += 1
+            else:
+                player_count += 1
+
+            if player_count == k:
+                k_count +=1
+
+    player_count = 0
+    #check vertically
+    for j in range(columns):
+        for i in range(rows):
+            if board.board[i][j] == player:
+                player_count += 1
+            else:
+                player_count += 1
+
+            if player_count == k:
+                k_count += 1
+
+
+    player_count = 0
+    # checks diagonally
+
+
+
+
+
+
+
+
+
+
+
+
+
